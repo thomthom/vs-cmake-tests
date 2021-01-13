7d32ab59f3071d774ce9652d7bbfe93cb0d84867
Thomas Thomassen <thomas@thomthom.net>
2021-01-13 22:11:21 +0100

    Multiple libs with the same name.

---
+++ example_lib.cpp
@@ -0,0 +1,9 @@
+#include "example_lib.h"
+
+namespace example {
+
+int add(int a, int b) {
+  return a + b;
+}
+
+} // namespace example
---
+++ example_lib.h
@@ -0,0 +1,5 @@
+namespace example {
+
+int add(int a, int b);
+
+} // namespace example
